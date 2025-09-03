6104789be74c9c5999361f1047b1fbd48f02b144
RavinduPerera <it22064622@my.sliit.lk>
2025-09-03 10:51:57 +0530

    add Camp component enhancements: include image rendering and dynamic title/subtitle display

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image'
+import { PEOPLE_URL } from '@/constants';
 
 interface CampProps {
   backgroundImage: string;
@@ -11,7 +13,40 @@ const CampSite = ({backgroundImage, title, subtitle, peopleJoined}: CampProps) =
   return (
       <div className='h-full w-full main-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl'>
         <div className={'flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10 border-2 border-blue-500'}>
+          <div className='flexCenter gap-4'>
+            {/* map image div */}
+            <div className='rounded-full bg-green-50 p4'>
+              <Image 
+              src='/folded-map.svg'
+              alt='map' 
+              width={28} 
+              height={28}
+              />
+            </div>
 
+            {/* image heading div */}
+            <div className='flex flex-col gap-1'>
+              <h4 className='bold-18 text-white'>{title}</h4>
+              <p className='regular-14 text-white'>{subtitle}</p>
+            </div>
+          </div>
+
+          {/* people joined div */}
+          <div className='flexCenter gap-6'>
+            <span className='flex -space-x-4 overflow-hidden'>
+              {PEOPLE_URL.map((url) => (
+                <Image className='inline-block h-10 w-10 rounded-full'
+                  src={url}
+                  key={url}
+                  alt='person'
+                  width={52}
+                  height={52}
+                />
+              ))}
+            </span>
+
+            <p className='bold-16 md:bold-20 text-white'>{peopleJoined}</p>
+          </div>
         </div>
       </div>
   )
@@ -33,6 +68,11 @@ function Camp() {
             subtitle='Principal Kubukui Camp'
             peopleJoined='100+ Joined'
         />
+      </div>
+
+      {/* green tag div on the images */}
+      <div>
+
       </div>
     </section>
   )
